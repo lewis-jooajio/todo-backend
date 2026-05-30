dfa8319770751e7e69da1796b4a1bbc257e12595
lewis-jooajio <yangkyu2207@gmail.com>
2026-05-30 16:43:53 +0900

    use DATABASE_URL for db connection

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 })
 
 app.use(cors())
